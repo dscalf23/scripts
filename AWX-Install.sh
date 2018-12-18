adc4764857f3737e4aad4c4c20fadf15d16c3195
David Scalf <davidscalf@Matthews-Mac-mini.local>
2018-12-17 18:07:26 -0700

    fixed distro

--- AWX-Install.sh
+++ AWX-Install.sh
@@ -29,7 +29,7 @@ DOMAIN=${FQDN#*.}
 apt update -y
 apt install -y apt-transport-https ca-certificates curl software-properties-common 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-add-repository --yes "deb [arch=amd64] https://download.docker.com/linux/ubuntu trusty stable"
+apt-add-repository --yes "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-add-repository --yes ppa:ansible/ansible
 add-apt-repository --yes universe
 apt update -y
