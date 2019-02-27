35541373ad64c72dd118486301fb420a7ee63c1e
davidscalf <david@izoox.com>
2019-02-27 10:20:43 -0700

    Finalized swapiness and renammed

--- Swapiness.py
+++ Swapiness.py
@@ -6,17 +6,18 @@
 #
 # Script for the removal of swap files and partitions and the calculation and creation of a swap file.
 
+# Imports
 import os
-import subprocess
 import psutil
 import shlex
-# import fstab
 
-# Stat of the Swappiness
+
+# Start of the Swappiness
 def start():
     os.system('sudo cp -f /etc/fstab /etc/fstab.old')
     if_swap()
 
+# Calc the new swap file
 def calc_swap():
     mem = psutil.virtual_memory().total
     if mem < 1000000000:
@@ -33,6 +34,7 @@ def calc_swap():
         swap = 16384
     create_swap(swap)
 
+# Create the new swap File
 def create_swap(size):
     os.system('sudo dd if=/dev/zero of=swapfile bs=1M count=' + str(size))
     os.system('sudo chmod 600 swapfile')
@@ -40,38 +42,31 @@ def create_swap(size):
     os.system('sudo swapon swapfile')
     os.system('sudo echo "/root/swapfile swap swap defaults 0 0" >> /etc/fstab')
 
+# Is swap setup already
 def if_swap():
     swap_list = [line.rstrip('\n') for line in open('/proc/swaps')]
-
     if swap_list[1] is not None:
         os.system('swapoff -a')
+        # Remove old Swap
         for counter, value in enumerate(swap_list[1:]):
-        # path = subprocess.check_output(['grep', 'swap', '/etc/fstab'])
             path = shlex.split(value)
-            print path[1]
-            print path[0]
             if path[1] == 'file':
-                print path[0]
                 os.system('sudo rm -fr ' + path[0])
-                #Fstab.remove_by_mountpoint(path[0])
-                os.system('sudo sed "\|^' + path[0] + '|d" /etc/fstab > /etc/fstab.tmp && sudo mv -f /etc/fstab.tmp /etc/fstab')
+                os.system("sudo sed '\|" + path[
+                    0] + "|d' /etc/fstab > /etc/fstab.tmp && sudo mv -f /etc/fstab.tmp /etc/fstab")
             elif path[1] == 'partition':
                 part = path[0]
-                print part
-                part = part[4:]
-                print part
+                part = part[5:]
                 id = part[:2]
-                print id
                 if id == 'dm':
                     os.system('ls -l /dev/mapper | grep ' + part + ' >> part_' + part + '.txt')
                     tmp = shlex.split(open('part_' + part + '.txt'))
-                    print tmp
+                    os.system("sudo sed '\|" + tmp[8] + "|d' /etc/fstab > /etc/fstab.tmp && sudo mv -f /etc/fstab.tmp /etc/fstab")
+                    os.system('sudo rm -fr part_' + part + '.txt')
+
                 else:
-                    print path[0]
-                    os.system('sudo sed "\|^' + path[0] + '|d" /etc/fstab > /etc/fstab.tmp && sudo mv -f /etc/fstab.tmp /etc/fstab')
-
-
-
+                    os.system("sudo sed '\|" + path[0] + "|d' /etc/fstab > /etc/fstab.tmp && sudo mv -f /etc/fstab.tmp /etc/fstab")
     calc_swap()
+
 
 start()
